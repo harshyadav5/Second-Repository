5810d5454a787130a437b8e5628fe8bf5b254d12
harshyadav5 <harsh.yadav@tothenew.com>
2020-03-25 19:39:43 +0530

    test this code

---
+++ Project-Demo-Assignment/src/main/java/com/example/ProjectDemoAssignment/ProjectDemoAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectDemoAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDemoAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDemoAssignmentApplication.class, args);
+	}
+
+}
---
+++ Project-Demo-Assignment/src/test/java/com/example/ProjectDemoAssignment/ProjectDemoAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectDemoAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDemoAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
